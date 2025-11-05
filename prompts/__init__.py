c9079533673f9755b0f875f7c013630982eb54fa
bogoconic1 <tqy8587@gmail.com>
2025-11-05 08:27:00 +0000

    drop debug and leakage guardrails (will be handled in search_red_flags)

--- prompts/__init__.py
+++ prompts/__init__.py
@@ -2,14 +2,12 @@ from . import developer_agent as developer_agent
 from . import researcher_agent as researcher_agent
 from . import tools_developer as tools_developer
 from . import tools_researcher as tools_researcher
-from . import guardrails as guardrails
 
 __all__ = [
     "developer_agent",
     "researcher_agent",
     "tools_developer",
     "tools_researcher",
-    "guardrails",
 ]
 
 
